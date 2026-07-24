3c83b30050a06efb6696344cee48efba7d4ca1d6
Boris Vergasov <starsfer839@yandex.ru>
2026-07-24 19:34:25 +0300

    изменил лимиттер

--- backend/src/middlewares/rateLimiter.ts
+++ backend/src/middlewares/rateLimiter.ts
@@ -1,26 +1,28 @@
-import rateLimit from 'express-rate-limit'
+import rateLimit  from 'express-rate-limit'
 
-//  Общий лимит для всех запросов
 export const globalLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 минута
-    max: 100, // максимум 100 запросов с одного IP
-    message: {
-        success: false,
-        error: 'Слишком много запросов. Пожалуйста, повторите позже.',
-    },
-    standardHeaders: true,
+    limit: 10, // В версии 8.x используется `limit`, а не `max`
+    standardHeaders: 'draft-7',
     legacyHeaders: false,
+    handler: (req, res) => {
+      res.status(429).json({
+      success: false,
+      error: "Слишком много запросов. Пожалуйста, повторите позже."
+    });
+  },
 })
 
 //  Строгий лимит для логина (защита от брутфорса)
 export const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 минут
-    max: 10, // максимум 10 попыток входа
+    windowMs: 15 * 1000, // 15 минут
+    limit: 10, // 10 попыток входа
     skipSuccessfulRequests: true,
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+    statusCode: 429,
     message: {
         success: false,
         error: 'Слишком много попыток входа. Попробуйте через 15 минут.',
     },
-    standardHeaders: true,
-    legacyHeaders: false,
 })
