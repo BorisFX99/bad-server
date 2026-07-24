d054dbcace0a9e625877186c0eeb6cb7303a1f1d
Boris Vergasov <starsfer839@yandex.ru>
2026-07-24 17:14:25 +0300

    изменил роут для csrf-token

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -12,6 +12,7 @@ import uploadRouter from './upload'
 const router = Router()
 
 router.get('/csrf-token', csrfProtection, getCsrfToken)
+router.get('/auth/csrf-token', csrfProtection, getCsrfToken)  // ← Добавить!
 
 router.use('/auth', authRouter)
 router.use('/product', productRouter)
